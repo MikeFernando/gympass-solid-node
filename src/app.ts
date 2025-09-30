db6bca678b203dcbbf16d3b99bd8534bc090383a
mikera <mikera2020@outlook.com>
2025-09-29 22:10:16 -0300

    Integração com Prisma ORM - Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import Fastify from 'fastify'
+import { PrismaClient } from './generated/prisma'
 
 export const app = Fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Mike Fernando',
+    email: 'mikefernando@gmail.com',
+  },
+})
