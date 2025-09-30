f90b73bd3c4e8478d81e257c380f958a3622bdf0
mikera <mikera2020@outlook.com>
2025-09-30 18:23:45 -0300

    Caso de Uso e Design Patterns - Caso de uso de registro

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
-import { hash } from 'bcryptjs'
 import { z } from 'zod'
 
-import { prisma } from 'lib/prisma'
+import { registerUseCase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const requestBodySchema = z.object({
@@ -13,33 +12,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = requestBodySchema.parse(request.body)
 
-  const password_hash = await hash(password, 6)
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
+  try {
+    await registerUseCase({ name, email, password })
+  } catch (error) {
     return reply.status(409).send()
   }
 
-  try {
-    await prisma.user.create({
-      data: {
-        name,
-        email,
-        password_hash,
-      },
-    })
-
-    return reply.status(201).send()
-  } catch (error) {
-    if (error instanceof Error && 'code' in error && error.code === 'P2002') {
-      return reply.status(409).send()
-    }
-
-    throw error
-  }
+  return reply.status(201).send()
 }
