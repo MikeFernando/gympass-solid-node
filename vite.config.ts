773119b4024a507889943b3e3a28e5fb91aba934
mikera <mikera2020@outlook.com>
2025-09-30 20:57:39 -0300

    Design Patterns & Testes - Gerando coverage de testes

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,18 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.spec.ts',
+        '**/*.test.ts',
+        'src/generated/**'
+      ]
+    }
+  }
 })
