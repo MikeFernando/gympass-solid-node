8b42db90122916b75b2ad18979b0680c9f238a68
mikera <mikera2020@outlook.com>
2025-09-30 21:21:03 -0300

    Implementando casos de uso - Caso de uso de autenticação

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,35 @@
+import { compare } from 'bcryptjs'
+
+import type { UsersRepository } from '@/repository/users-repositoru'
+import { InvalidCredentialsError } from './erros/invalid-credentials-error'
+
+import type { User } from '@/generated/prisma'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+type AuthenticateUseCaseResponse = {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) { }
+
+  async execute({ email, password }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatch = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatch) {
+      throw new InvalidCredentialsError()
+    }
+
+    return { user }
+  }
+}
--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -2,8 +2,8 @@ import { compare } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
 
 import { RegisterUseCase } from './register'
-import { UserAlreadyExistsError } from '../erros/user-already-exists-error'
-import { InMemoryUsersRepository } from '../../repository/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './erros/user-already-exists-error'
+import { InMemoryUsersRepository } from '../repository/in-memory/in-memory-users-repository'
 
 describe('Register use case', () => {
 
